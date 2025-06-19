e66b1e411bf4cc6648f3031cd790daca0569c9c7
AngelMunguiaR <324248540@alumno.enp.unam.mx>
2025-06-19 12:37:14 -0600

    feat:creacion de buscador, identifica la posicion del texto dado por el usuario

---
+++ Dynamics/JS/juego.js
@@ -0,0 +1,24 @@
+let texto_inp=document.getElementById("texto-input");
+let busca_inp=document.getElementById("busca-input");
+let eject_bot=document.getElementById ("buscar-btn");
+let resul_pant=document.getElementById("result-pantalla");
+
+eject_bot.addEventListener('click',function(){
+        console.log("hola");
+
+    let texto = texto_inp.value;
+    let buscar = busca_inp.value;
+    let i=0;
+    let separa=texto.split(buscar);
+    let cont=separa.size-1;
+    while(i< texto.length && i!=-1)
+    {
+        i=texto.indexOf(buscar,i);
+        if(i!=-1){
+            resul_pant.innerHTML += i + ",";
+            i++;
+        }
+    }
+
+    
+});
