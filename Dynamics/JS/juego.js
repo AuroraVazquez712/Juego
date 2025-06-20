9df8f99c0596534f2e5a5b91e621c57a787e4284
leileni <324156229@alumno.enp.unam.mx>
2025-06-19 22:56:47 -0600

    fix:convertir letras mayusculas ingresadas en busqueda a letras minusculas

--- Dynamics/JS/juego.js
+++ Dynamics/JS/juego.js
@@ -4,17 +4,19 @@ let eject_bot=document.getElementById ("buscar-btn");
 let resul_pant=document.getElementById("result-pantalla");
 
 eject_bot.addEventListener('click',function(){
-        console.log("hola");
-
+    
     let texto = texto_inp.value;
     let buscar = busca_inp.value;
+    //Convertir a minúsculas
+    let texto_min = texto.toLowerCase();
+    let buscar_min = buscar.toLowerCase();
     let i=0;
-    let separa=texto.split(buscar);
+    let separa=texto_min.split(buscar_min);
     let cont=separa.length-1;
     
-    while(i< texto.length && i!=-1)
+    while(i< texto_min.length && i!=-1)
     {
-        i=texto.indexOf(buscar,i);
+        i=texto_min.indexOf(buscar_min,i);
         if(i!=-1){
             resul_pant.innerHTML += i + ",";
             i++;
